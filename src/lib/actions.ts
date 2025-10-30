da23e9d6778d3241d9d93a526844be4fefc6379e
Swaliha Mulla <swaliharoonmulla192@gmail.com>
2025-10-30 03:55:36 +0000

    Try fixing this error: `Runtime Error: TypeError: Cannot read properties

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,7 +3,10 @@
 import { redirect } from 'next/navigation';
 import { managers, staffMembers } from './data';
 
-export async function authenticateManager(formData: FormData) {
+export async function authenticateManager(
+  previousState: { error: string } | undefined,
+  formData: FormData
+) {
   const managerId = formData.get('id') as string;
   const isValid = managers.some((manager) => manager.id === managerId.toUpperCase());
   
@@ -14,7 +17,10 @@ export async function authenticateManager(formData: FormData) {
   }
 }
 
-export async function authenticateStaff(formData: FormData) {
+export async function authenticateStaff(
+  previousState: { error: string } | undefined,
+  formData: FormData
+) {
   const staffId = formData.get('id') as string;
   const isValid = staffMembers.some((staff) => staff.id === staffId.toUpperCase());
   
